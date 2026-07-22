3e7d6a6278b10bf61cc10124d7e8c5a0aaed31de
pavivengat787-sys <pavivengat787@gmail.com>
2026-07-22 14:52:00 +0530

    Add files via upload

---
+++ Tollapi/src/main/java/com/sfl/tollapi/TollapiApplication.java
@@ -0,0 +1,13 @@
+package com.sfl.tollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TollapiApplication.class, args);
+    }
+
+}
